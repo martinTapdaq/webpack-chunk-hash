343f161c6a9d0e50dcdb2534716a8affdd031ccb
Martin Babinec <martin@tapdaq.com>
2018-03-29 15:26:07 -0500

    Fix webpack 4 deprecation warning

--- index.js
+++ index.js
@@ -15,11 +15,40 @@ WebpackChunkHash.prototype.apply = function(compiler)
 {
   var _plugin = this;
 
-  compiler.plugin('compilation', function(compilation)
-  {
-    compilation.plugin('chunk-hash', function(chunk, chunkHash)
+  var compilerPlugin;
+  var compilationPlugin;
+  if (compiler.hooks) {
+    compilerPlugin = function (fn)
     {
-      var modules = chunk.mapModules ? chunk.mapModules(getModuleSource) : chunk.modules.map(getModuleSource)
+      compiler.hooks.compilation.tap('WebpackChunkHash', fn);
+    };
+    compilationPlugin = function (compilation, fn)
+    {
+      compilation.hooks.chunkHash.tap('WebpackChunkHash', fn);
+    }
+  } else {
+    compilerPlugin = function (fn)
+    {
+      compiler.plugin('compilation', fn);
+    };
+    compilationPlugin = function (compilation, fn)
+    {
+      compilation.plugin('chunk-hash', fn);
+    }
+  }
+
+  compilerPlugin(function(compilation)
+  {
+    compilationPlugin(compilation, function(chunk, chunkHash)
+    {
+      var modules;
+      if (chunk.modulesIterable) {
+        modules = Array.from(chunk.modulesIterable, getModuleSource);
+      } else if (chunk.mapModules) {
+        modules = chunk.mapModules(getModuleSource);
+      } else {
+        modules = chunk.modules.map(getModuleSource);
+      }
       var source = modules.sort(sortById).reduce(concatenateSource, '')
         , hash   = crypto.createHash(_plugin.algorithm).update(source + _plugin.additionalHashContent(chunk))
         ;
